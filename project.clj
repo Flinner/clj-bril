d01fd2ea83b4009bbef718559a2034b26a182402
Flinner Yuu <flinner@nand.sh>
2025-06-26 14:35:14 +0300

    doc: add project description

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject bril-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "bril (Big Red IR) compiler, following through Cornell's CS6120 course"
+  :url "https://github.com/Flinner/clj-bril"
+  :license {:name "AGPL-3.0"
+            :url "https://www.gnu.org/licenses/agpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/data.json "2.4.0"]]
                  
