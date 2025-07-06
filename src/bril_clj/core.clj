0331dfc8017d697dbc5aad9b2be4d14c9f78acb1
Flinner Yuu <flinner@nand.sh>
2025-07-06 20:42:02 +0300

    feat: working example

--- src/bril_clj/core.clj
+++ src/bril_clj/core.clj
@@ -14,33 +14,34 @@
   [& args]
   (println "Hello, World!"))
 
-(def bril-json (bril/txt->json "./test/bril_files/parse/positions.bril"))
-(def cfg (bril/json->cfg bril-json))
+;; (def bril-json (bril/txt->json "./test/bril_files/parse/positions.bril"))
+;; (def cfg (bril/json->cfg bril-json))
 
-(def body
-  [{:label :b0,
-    :succ [:label],
-    :block
-    [{:dest "a", :op "const", :type "int", :value 4}
-     {:dest "b", :op "const", :type "int", :value 4}
-     {:args ["a" "b"], :dest "sum1", :op "add", :type "int"}
-     {:dest "a", :op "const", :type "int", :value 8}
-     {:args ["a" "b"], :dest "sum2", :op "add", :type "int"}
-     {:args ["sum1" "sum2"], :dest "prod", :op "mul", :type "int"}
-     {:args ["prod"], :op "print"}
-     {:label :b1, :succ [:label], :block []}]}
-   {:label :label,
-    :succ [nil],
-    :block
-    [{:label "label"}
-     {:args ["v0" "v1"], :dest "v2", :op "add", :type "int"}
-     {:args ["v2"], :op "print"}]}])
+;; (def body
+;;   [{:label :b0,
+;;     :succ [:label],
+;;     :block
+;;     [{:dest "a", :op "const", :type "int", :value 4}
+;;      {:dest "b", :op "const", :type "int", :value 4}
+;;      {:args ["a" "b"], :dest "sum1", :op "add", :type "int"}
+;;      {:dest "a", :op "const", :type "int", :value 8}
+;;      {:args ["a" "b"], :dest "sum2", :op "add", :type "int"}
+;;      {:args ["sum1" "sum2"], :dest "prod", :op "mul", :type "int"}
+;;      {:args ["prod"], :op "print"}
+;;      {:label :b1, :succ [:label], :block []}]}
+;;    {:label :label,
+;;     :succ [nil],
+;;     :block
+;;     [{:label "label"}
+;;      {:args ["v0" "v1"], :dest "v2", :op "add", :type "int"}
+;;      {:args ["v2"], :op "print"}]}])
 
 (->> "./test/bril_files/parse/positions.bril"
      bril/txt->json
      bril/json->cfg
-     ((partial opt/apply-block-optimization-to-cfg-once optlvn/lvn))
-     ((partial opt/apply-function-optimization-once optdc/function|DCE-unused-variable-declarations))
+     ((partial opt/apply-block-optimization-to-cfg-once          optlvn/lvn))
+     ((partial opt/apply-block-optimization-until-convergence    optdc/block|DCE-double-assignment))
+     ((partial opt/apply-function-optimization-until-convergence optdc/function|DCE-unused-variable-declarations))
      bril/cfg->json
      bril/json->txt
      println)
